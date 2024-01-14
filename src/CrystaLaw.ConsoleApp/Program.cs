50ba603f18dba23a5c17625e40268290a8d8e47d
Jackson Guizzo Zanette <BRMOU0092@ambev.com.br>
2024-01-14 17:43:35 -0300

    feat: change path and file name
    * add result files to root directory

--- src/CrystaLaw.ConsoleApp/Program.cs
+++ src/CrystaLaw.ConsoleApp/Program.cs
@@ -21,14 +21,14 @@ namespace CrystaLaw.ConsoleApp
             var legislatorsService = serviceProvider.GetRequiredService<ILegislatorsService>();
             var legislatorsVotesCountResult = await legislatorsService.GetLegislatorsWithVoteCountsAsync(CancellationToken.None);
 
-            var relativePath = $"Data{Path.DirectorySeparatorChar}legislators-support-oppose-count.csv";
+            var relativePath = $"legislators-support-oppose-count.csv";
             CSVParser.WriteRecords(relativePath, legislatorsVotesCountResult);
             Console.WriteLine($"File '{Directory.GetCurrentDirectory()}{Path.DirectorySeparatorChar}{relativePath}' generated");
 
             var billsService = serviceProvider.GetRequiredService<IBillsService>();
             var billsVotesCountResult = await billsService.GetBillsWithVouteCountsAsync(CancellationToken.None);
 
-            relativePath = $"Data{Path.DirectorySeparatorChar}bills-supported-opposed-count.csv";
+            relativePath = $"bills.csv";
             CSVParser.WriteRecords(relativePath, billsVotesCountResult);
             Console.WriteLine($"File '{Directory.GetCurrentDirectory()}{Path.DirectorySeparatorChar}{relativePath}' generated");
         }
