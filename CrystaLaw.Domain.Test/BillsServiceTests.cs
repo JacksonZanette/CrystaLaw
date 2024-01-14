d97e7f1ef7cdf7f5725b092ff6bbc05d3d5aebe0
Jackson Guizzo Zanette <BRMOU0092@ambev.com.br>
2024-01-14 12:05:53 -0300

    test: clean initial test wip to ensure application being built

--- CrystaLaw.Domain.Test/BillsServiceTests.cs
+++ CrystaLaw.Domain.Test/BillsServiceTests.cs
@@ -4,20 +4,14 @@ namespace CrystaLaw.Domain.Test
     {
         public BillsServiceTests()
         {
-            _autoMocker = new AutoMocker();
         }
 
         [Fact]
         public void BillsVoteResults_WhenRequested_ShouldReturnDataSet()
         {
             //Act
-            var service = CreateService();
-
             //Arrange
-
             //Assert
         }
-
-        private object CreateService() => throw new NotImplementedException();
     }
 }
